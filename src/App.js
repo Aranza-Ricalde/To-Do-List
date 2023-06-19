a492ca9ebbfe8cab4f57075903a4e291c9773c4d
AranzaRicalde <A17003656@alumnos.uady.mx>
2023-06-18 19:06:58 -0600

    Upload project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoItem } from './TodoItem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
+
+      <CreateTodoButton />
+    </div>
+  );
+}
+
+
+export default App;
